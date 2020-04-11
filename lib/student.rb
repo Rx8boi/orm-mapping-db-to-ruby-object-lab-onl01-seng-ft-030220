5e597c57a08517e54de096d3792b30f8911a7491
Charles Butler <cdbutle@outlook.com>
2020-04-11 22:39:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
   WHERE grade = 9
   SQL
 
-  DB[:conn].execute(sql, grade).map do |row|
+  DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
   end.first
     # find the student in the database given a name
