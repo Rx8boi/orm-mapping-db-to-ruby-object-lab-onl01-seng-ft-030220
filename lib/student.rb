2828de1db17fd589f39de548c1843d38743a9014
Charles Butler <cdbutle@outlook.com>
2020-04-11 22:33:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,13 +23,13 @@ class Student
   end
 
   def self.find_by_name(name)
-  self.find_by_name(name) = find
   sql = <<-SQL
   SELECT name
   FROM students
   WHERE name = ?
   LIMIT 1
   SQL
+  
   DB[:conn].execute(sql, name).map do |row|
     self.new_from_db(row)
   end.first
